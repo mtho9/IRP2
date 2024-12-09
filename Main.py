532bbc46971501f315d086742fadc4a999f9f185
mho9 <mandy.ho@maine.edu>
2024-12-09 17:57:42 -0500

    includes main function to run new SVM prepared dataset

--- Main.py
+++ Main.py
@@ -1,20 +1,18 @@
 from DataRetrieval import fetch_trends_data
-from DataAnalysis import analyze_seasonality_acf, combine_data
+import DataAnalysis
+import Classification
 
 def main():
-    # # Fetch trends data for all periods
-    # fetch_trends_data('queries.csv', '2004-01-01', '2024-01-01', 'trends_data_monthly_2004_2024.csv')
-    # fetch_trends_data('queries.csv', '2004-01-01', '2020-02-29', 'pre_pandemic_trends_data.csv')
-    # fetch_trends_data('queries.csv', '2021-04-01', '2024-01-01', 'post_pandemic_trends_data.csv')
+    file_path = "all_trends_data.xlsx"  # Replace with your Excel file path
+    output_path = "prepared_SVM_dataset.csv"  # Desired output CSV file path
 
-    # Perform analysis (Seasonality and ACF) on full data set
-    # analyze_seasonality_acf('all_trends_data.xlsx', 'seasonality_acf_results.csv')
+    df = DataAnalysis.load_and_prepare_data(file_path)
 
-    # Combine pre-pandemic and post-pandemic data
-    # combine_data('pre_pandemic_trends_data.csv', 'post_pandemic_trends_data.csv', 'combined_trends_data.csv')
+    monthly_means = DataAnalysis.calculate_monthly_means(df)
 
-    # Perform analysis on combined data set with no pandemic
-    analyze_seasonality_acf('combined_trends_data.csv', 'seasonality_acf_results_without_pandemic.csv')
+    dataset = DataAnalysis.prepare_dataset_for_svm(monthly_means, labels=None)
+
+    DataAnalysis.save_dataset_to_csv(dataset, output_path)
 
 if __name__ == "__main__":
     main()
